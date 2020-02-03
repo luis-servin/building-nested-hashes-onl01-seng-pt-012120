b35557bd0a87d9f9ce9aac1dac5fca7a1719ee3a
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 22:03:01 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   #code your solution here:
-  epic_tragedy[:montague][:patriarch][:status] = "dead"
-  
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
